0c8ee7ff34c0e5cc8638021970c11493ef4fc8cf
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-21 23:04:50 -0500

    Completed first pass of 'player move' logic (eg. handling keyboard input).

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
 ( function( window, document ) {
-    console.log( 'INSIDE `main.js` IIFE.' );
+
+    // DECLARE VARS
+    var player = document.getElementsByClassName( 'player' )[ 0 ];
+
+    // DECLARE FUNCTIONS
+    function movePlayer( player, direction ) {
+        var prop = null;
+        var mod = 1;
+        var val = null;
+
+        switch ( direction ) {
+            case 'up':
+                mod = -1;
+            case 'down':
+                console.log( 'MATCHED "up" || "down".' ); /// TEMP
+                console.log( player.style.top ); /// TEMP
+                prop = 'top';
+
+                break;
+            case 'left':
+                mod = -1;
+            case 'right':
+                console.log( 'MATCHED "left" || "right".' ); /// TEMP
+                console.log( player.style.left ); /// TEMP
+                prop = 'left';
+
+                break;
+            default:
+                // DO NO THINGS;
+        }
+
+        val = parseInt( player.style[ prop ] ) || 0;
+
+        player.style[ prop ] = ( val + ( mod * 100 ) ) + 'px';
+    }
+
+    // EVENTS
+    window.addEventListener( 'keyup', function( e ) {
+        switch ( e.keyCode ) {
+            case 37:
+                console.log( 'MOVE LEFT' ); /// TEMP
+                movePlayer( player, 'left' );
+
+                break;
+            case 38:
+                console.log( 'MOVE UP' ); /// TEMP
+                movePlayer( player, 'up' );
+
+                break;
+            case 39:
+                console.log( 'MOVE RIGHT' ); /// TEMP
+                movePlayer( player, 'right' );
+
+                break;
+            case 40:
+                console.log( 'MOVE DOWN' ); /// TEMP
+                movePlayer( player, 'down' );
+
+                break;
+            default:
+                // DO NO THINGS;
+        }
+    } );
 } )( window, document );
