fca12690973b411c0174b1afab0b5751ec0134fa
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-23 22:55:36 -0500

    Added 'Terrain' entity to project.

---
+++ src/js/terrain.js
@@ -0,0 +1,11 @@
+function Terrain( options ) {
+    this.id = ( Math.floor( Math.random() * 1000000 ) + '' ).substring( 0, 5 );
+
+    return this;
+}
+
+
+// --------------------------------------------------
+// PUBLIC API
+// --------------------------------------------------
+module.exports = Terrain;
