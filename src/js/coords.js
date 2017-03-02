8bcc539df3704353d31724d2a28ccb9d675478f1
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-01 23:19:06 -0500

    Added 'get' and 'set' methods to 'Coords' entity.

--- src/js/coords.js
+++ src/js/coords.js
@@ -2,7 +2,53 @@
 // DECLARE FUNCTIONS
 // --------------------------------------------------
 function Coords( options ) {
+    options = options || {};
+
+    this.coords = options.coords; /// TEMP
+
     return this;
+}
+
+
+Coords.prototype.get = function( options ) {
+    options = options || {};
+
+    if ( !options.offset || typeof options.offset !== 'object' ) {
+        return this.coords;
+    }
+
+    var targetIndex = null;
+    var newCoords = this.coords.slice( 0 );
+
+    switch ( options.offset.direction ) {
+        case 'up':
+        case 'down':
+            targetIndex = 0;
+            break;
+        case 'left':
+        case 'right':
+            targetIndex = 1;
+            break;
+        default:
+            // DO NO THINGS;
+    }
+
+    newCoords[ targetIndex ] = ( newCoords[ targetIndex ] + options.offset.distance );
+
+    return newCoords;
+} // /get()
+
+
+Coords.prototype.set = function( coords ) {
+    coords = coords || null;
+
+    if ( !coords || !Array.isArray( coords ) || !coords.length ) {
+        return null;
+    }
+
+    this.coords = coords;
+
+    return true;
 }
 
 
